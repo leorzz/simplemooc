db26422f17ce7180634db79cd97028b823a024cf
leorzz <rizzi.leo@gmail.com>
2017-11-30 12:15:08 +0000

    antes do udemy

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import include, url
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,5 +12,11 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-
+if settings.DEBUG:
+    #document_root=settings.MEDIA_ROOT = caminho absoluto
+    #settings.MEDIA_URL = caminho relativo
+    #este método funciona apenas de "debug mode"
+    #indica qual é o caminho absoluto e relativo para estas midias
+    #https://docs.djangoproject.com/en/dev/howto/static-files/
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
